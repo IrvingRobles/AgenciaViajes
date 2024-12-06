874913df4188e8a5006adb4e332aeb0d046233a2
IrvingRobles <98172734+IrvingRobles@users.noreply.github.com>
2024-12-06 03:32:54 -0600

    Update server.js

--- server.js
+++ server.js
@@ -1,15 +1,16 @@
 const express = require('express');
-const path = require('path');
+const cors = require('cors');
 const session = require('express-session');
-const db = require('./models/db'); // Conexión a la base de datos
-const routes = require('./routes');
+const routes = require('./routes'); // Aquí se define que las rutas serán solo en '/api'
 require('dotenv').config();
 
 const app = express();
+
+// Middleware de configuración
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-const cors = require('cors');
+// Configuración CORS para permitir solicitudes desde cualquier origen
 app.use(cors({
   origin: '*', // Permite solicitudes desde cualquier origen
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Métodos permitidos
@@ -24,11 +25,11 @@ app.use(session({
   cookie: { secure: false } // Si es HTTP, debe ser false
 }));
 
-// Middleware de rutas para la API
+// Rutas de la API en la ruta raíz '/api'
 app.use('/api', routes);
 
 // Iniciar el servidor
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Servidor corriendo en el puerto ${PORT}`);
+  console.log(`Servidor API corriendo en el puerto ${PORT}`);
 });
